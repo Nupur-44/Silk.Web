49e164e7c591f54e277f24d99b75cfb7a48a6550
John Carruthers <johnc@frag-labs.com>
2019-03-10 14:47:57 -0500

    Add APIs for customizing object modelling rules.

---
+++ Silk.Web/ObjectMapping/MappingOptions.cs
@@ -0,0 +1,37 @@
+﻿using Silk.Data.Modelling;
+using Silk.Data.Modelling.Analysis.CandidateSources;
+using Silk.Data.Modelling.Analysis.Rules;
+using System.Collections.Generic;
+
+namespace Silk.Web.ObjectMapping
+{
+	public class MappingOptions
+	{
+		public List<IIntersectCandidateSource<TypeModel, PropertyInfoField, TypeModel, PropertyInfoField>>
+			IntersectCandidateSources { get; } = new List<IIntersectCandidateSource<TypeModel, PropertyInfoField, TypeModel, PropertyInfoField>>();
+
+		public List<IIntersectionRule<TypeModel, PropertyInfoField, TypeModel, PropertyInfoField>>
+			IntersectionRules { get; } = new List<IIntersectionRule<TypeModel, PropertyInfoField, TypeModel, PropertyInfoField>>();
+
+		public MappingOptions()
+		{
+			IntersectCandidateSources.AddRange(GetDefaultCandidateSources());
+			IntersectionRules.AddRange(GetDefaultRules());
+		}
+
+		private static IEnumerable<IIntersectionRule<TypeModel, PropertyInfoField, TypeModel, PropertyInfoField>> GetDefaultRules()
+		{
+			yield return new SameDataTypeRule<TypeModel, PropertyInfoField, TypeModel, PropertyInfoField>();
+			yield return new BothNumericTypesRule<TypeModel, PropertyInfoField, TypeModel, PropertyInfoField>();
+			yield return new ConvertableWithToStringRule<TypeModel, PropertyInfoField, TypeModel, PropertyInfoField>();
+			yield return new ExplicitCastRule<TypeModel, PropertyInfoField, TypeModel, PropertyInfoField>();
+			yield return new ConvertableWithTryParse<TypeModel, PropertyInfoField, TypeModel, PropertyInfoField>();
+		}
+
+		private static IEnumerable<IIntersectCandidateSource<TypeModel, PropertyInfoField, TypeModel, PropertyInfoField>> GetDefaultCandidateSources()
+		{
+			yield return new ExactPathMatchCandidateSource<TypeModel, PropertyInfoField, TypeModel, PropertyInfoField>();
+			yield return new FlattenedNameMatchCandidateSource<TypeModel, PropertyInfoField, TypeModel, PropertyInfoField>();
+		}
+	}
+}
--- Silk.Web/ObjectModellingStartupExtensions.cs
+++ Silk.Web/ObjectModellingStartupExtensions.cs
@@ -1,5 +1,10 @@
-﻿using Silk.Data.Modelling;
+﻿using Microsoft.Extensions.Options;
+using Silk.Data.Modelling;
+using Silk.Data.Modelling.Analysis;
+using Silk.Data.Modelling.Analysis.CandidateSources;
+using Silk.Data.Modelling.Analysis.Rules;
 using Silk.Web.ObjectMapping;
+using System;
 
 namespace Microsoft.Extensions.DependencyInjection
 {
@@ -14,9 +19,37 @@ namespace Microsoft.Extensions.DependencyInjection
 		{
 			services.AddDefaultMappingDependencyServices();
 
-			services.AddSingleton(sP => new MappingStore());
+			services.AddSingleton(MappingStoreFactory);
 			services.AddScoped<IObjectMapper, WebObjectMapper>();
 			return services;
+		}
+
+		public static IServiceCollection AddObjectMapperRule(this IServiceCollection services,
+			IIntersectionRule<TypeModel, PropertyInfoField, TypeModel, PropertyInfoField> rule)
+		{
+			services.Configure<MappingOptions>(
+				options => options.IntersectionRules.Add(rule)
+				);
+			return services;
+		}
+
+		public static IServiceCollection AddObjectMapperFieldSource(this IServiceCollection services,
+			IIntersectCandidateSource<TypeModel, PropertyInfoField, TypeModel, PropertyInfoField> source)
+		{
+			services.Configure<MappingOptions>(
+				options => options.IntersectCandidateSources.Add(source)
+				);
+			return services;
+		}
+
+		private static MappingStore MappingStoreFactory(IServiceProvider serviceProvider)
+		{
+			var mappingOptions = serviceProvider.GetRequiredService<IOptions<MappingOptions>>().Value;
+			return new MappingStore(
+				new DefaultIntersectionAnalyzer<TypeModel, PropertyInfoField, TypeModel, PropertyInfoField>(
+					mappingOptions.IntersectCandidateSources, mappingOptions.IntersectionRules
+					)
+				);
 		}
 	}
 }
